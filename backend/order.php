a6cd12b6a238906e4a99ccd21324469150232a04
Zidan <b93605079@g.ntu.edu.tw>
2020-07-29 10:00:04 +0800

    訂票，自行塞serialize資料

--- backend/order.php
+++ backend/order.php
@@ -5,18 +5,33 @@ include_once "../base.php";
 <h3 class="ct">訂單清單</h3>
 <div>
   快速刪除:
-  <input type="radio" name="bydate" id="bydate">
+  <input type="radio" name="type" id="bydate">
   依日期
   <input type="text" name="date" id="date">
   依電影
-  <input type="radio" name="bymovie" id="bymovie">
+  <input type="radio" name="type" id="bymovie">
   <select name="movie" id="movie">
-
+  <?php
+    $mlist=$Ord->all([]," group by `movie`");
+    foreach($mlist as $m){
+      echo "<option value='".$m['movie']."'>".$m['movie']."</option>";
+    }
+  ?>
   </select>
-  <button>刪除</button>
+  <button onclick="qDel()">刪除</button>
   
 </div>
-<ul>
+<style>
+  ul{
+    display:flex;
+    list-style-type: none;
+    padding: 0;
+  }
+  ul li{
+    width: 14%;
+  }
+</style>
+<ul style="background:#eee">
   <li>訂單編號</li>
   <li>電影名稱</li>
   <li>日期</li>
@@ -38,15 +53,52 @@ foreach($orders as $ord){
   <li><?=$ord['date'];?></li>
   <li><?=$ord['session'];?></li>
   <li><?=$ord['qt'];?></li>
-  <li><?=$ord['seat'];?></li>
-  <li><button>刪除</button></li>
+  <li>
+    <?php
+  
+    $seat=unserialize($ord['seat']);
+    foreach($seat as $s){
+      echo floor($s/5)+1;
+      echo "排";
+      echo $s%5+1;
+      echo "號";
+    }
+    
+    ?>
+  
+  </li>
+  <li><button onclick="del('ord',<?=$ord['id'];?>)">刪除</button></li>
 </ul>
 
 <?php
 }
-
-
 ?>
+
+
 <div>
 
 </div>
+
+<script>
+  function del(table,id){
+    if(confirm("你確定要刪除該筆資料嗎?")){
+      $.post("api/del.php",{table,id},function(){
+        location.reload();
+      })
+    }
+  }
+
+
+  function qDel(){
+    let type=$("input[name='type']:checked").val()
+    switch(type){
+      case 'date':
+        let date=$("#bydate").val()
+      break;
+      case 'movie':
+        let movie=$("#bymovie").val()
+      break;
+    }
+  }
+
+</script>
